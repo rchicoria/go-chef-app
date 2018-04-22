fa328b75e9fff8de742f29d57385ce6cbbc4414f
Rui Chicória <rchicoria@gmail.com>
2018-04-22 08:00:06 +0100

    Implemented Share Modal

--- GoChefApp/Ext.js
+++ GoChefApp/Ext.js
@@ -1,29 +1,36 @@
-import { View,	Navigator,	StyleSheet,	TouchableOpacity,	Image,	Dimensions,	BackAndroid,	Platform,	ToastAndroid,	Text,	Alert, Linking, Button } from 'react-native'
+import { View, Navigator, StyleSheet, TouchableOpacity, Image, Dimensions, BackAndroid, Platform, ToastAndroid, Text, Alert, Linking, Button } from 'react-native'
 import React, { Component } from 'react'
 
 import ShareExtension from 'react-native-share-extension';
+import ShareModal from './lib/go-chef-design-system/lib/templates/ShareModal';
 
 export default class Index extends Component {
-    constructor(props){
+    constructor(props) {
         super(props)
         this.state = {}
     }
-	render(){
-		return(
-            <View style={{ flex: 1,
-                    flexDirection: 'column',
-                    justifyContent: 'center',
-                    alignItems: 'center',
-                    width: "100%",
-                    height: "100%",
-                    backgroundColor:'rgba(0,0,0, 0.3)'}}>
-			    <View style={{ height:200, width:'90%', backgroundColor:'deeppink' }} >
-                    <Button onPress={async () => {
-                        const { type, value } = await ShareExtension.data()
-                        ShareExtension.openURL("gochefapp://recipe?url="+value).catch(err => Alert.alert("ERRO"));
-                    }} title="Cenas"/>
-                </View>
-            </View>
-		)
-	}
-}
+    render() {
+        return (
+            <ShareModal onPress={
+                async () => {
+                    const { type, value } = await ShareExtension.data()
+                    ShareExtension.openURL("gochefapp://recipe?url=" + value).catch(err => Alert.alert("ERRO"));
+                }
+            } />
+            // <View style={{ flex: 1,
+            //         flexDirection: 'column',
+            //         justifyContent: 'center',
+            //         alignItems: 'center',
+            //         width: "100%",
+            //         height: "100%",
+            //         backgroundColor:'rgba(0,0,0, 0.3)'}}>
+            //     <View style={{ height:200, width:'90%', backgroundColor:'deeppink' }} >
+            //         <Button onPress={async () => {
+            //             const { type, value } = await ShareExtension.data()
+            //             ShareExtension.openURL("gochefapp://recipe?url="+value).catch(err => Alert.alert("ERRO"));
+            //         }} title="Cenas"/>
+            //     </View>
+            // </View>
+        )
+    }
+}   
